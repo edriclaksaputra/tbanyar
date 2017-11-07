56c1f334b9f3c6212c61767868831cfa558ad124
Edric Laksa Putra <Edric Laksa Putra>
2017-11-07 22:53:33 +0700

    7 Nov, Detail item seeder mau diisi

---
+++ tbanyar/database/seeds/DetailItemSeeder.php
@@ -0,0 +1,133 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DetailItemSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	//Holis Jaya
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>2,
+        // 	'id_item'=>79,
+        // 	'hargabeli'=>8950,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>2,
+        // 	'id_item'=>77,
+        // 	'hargabeli'=>5450,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>2,
+        // 	'id_item'=>78,
+        // 	'hargabeli'=>7075,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>2,
+        // 	'id_item'=>80,
+        // 	'hargabeli'=>12125,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+
+        //Senjaya
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>11,
+        // 	'id_item'=>55,
+        // 	'hargabeli'=>50250,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>11,
+        // 	'id_item'=>52,
+        // 	'hargabeli'=>20600,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>11,
+        // 	'id_item'=>29,
+        // 	'hargabeli'=>70900,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>11,
+        // 	'id_item'=>23,
+        // 	'hargabeli'=>11550,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+
+        // //PT.Catur
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>8,
+        // 	'id_item'=>19,
+        // 	'hargabeli'=>51700,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+
+        // //Holcim
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>7,
+        // 	'id_item'=>20,
+        // 	'hargabeli'=>51500,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+
+        // //Sinar Makmur
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>16,
+        // 	'id_item'=>1,
+        // 	'hargabeli'=>310000,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>16,
+        // 	'id_item'=>2,
+        // 	'hargabeli'=>353500,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+
+        // //Chrysta Mentari
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>1,
+        // 	'id_item'=>26,
+        // 	'hargabeli'=>32200,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+        // DB::table('detailitem')->insert([
+        // 	'id_supplier'=>1,
+        // 	'id_item'=>25,
+        // 	'hargabeli'=>22650,
+        // 	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // 	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        // ]);
+
+    	//Sheet 4
+        DB::table('detailitem')->insert([
+        	'id_supplier'=>11,
+        	'id_item'=>55,
+        	'hargabeli'=>50250,
+        	'created_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        	'updated_at'=>Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
+    }
+}
